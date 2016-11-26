30eb57636539f5e4b7e58a8d34077d8f029dd6a1
Tom Allen <tomjamesallenweb@gmail.com>
2016-11-26 23:20:56 +0000

    Adding `bem-class-generator`.

--- src/js/components/App.react.js
+++ src/js/components/App.react.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import CounterContainer from '../containers/CounterContainer'
 import GithubUserContainer from '../containers/GithubUserContainer'
+import bemClassGenerator from 'bem-class-generator';
+
+const bem = bemClassGenerator('app');
 
 const App = () => (
-  <div>
-    <h1>Redux Boilerplate</h1>
+  <div className={bem()}>
+    <h1 className={bem('header')}>Redux Boilerplate</h1>
     <CounterContainer />
     <GithubUserContainer />
   </div>
